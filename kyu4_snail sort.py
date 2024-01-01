4fa98f858b3f23be074f1d1ae4d86aa7a4072e10
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2024-01-01 18:48:25 +0700

    solved snail sort

--- kyu4_snail sort.py
+++ kyu4_snail sort.py
@@ -1,38 +1,31 @@
 def snail(snail_map):
-    leng=len(snail_map)
+    if(snail_map==[[]]): return []
     cont=snail_map
     out=[]
-    c=0
-    while c<1:
+    while True:
+        if(len(cont)==0): return out
+        elif(len(cont[0])==1): 
+            out.append(cont[0][0])
+            return out
+        
         for i in cont[0]:
             out.append(i)
 
         cont.pop(0)
 
-        for i in range(len(cont[cont-1])-1):
-            out.append(cont[i][leng-1])
-            cont[i].pop(leng-1)
+        for i in range(len(cont)):
+            out.append(cont[i][len(cont[i])-1])
+            cont[i].pop(len(cont[i])-1)
 
-        
-        for i in range(len(cont[len(cont)-1])):
-            out.append(cont[len(cont)-1][-i+1])
+        for i in range(len(cont)):                
+            out.append(cont[-1][-1])
+            cont[-1].pop(-1)   
         
         cont.pop(len(cont)-1)
+       
 
+        for i in range(len(cont)):
+            out.append(cont[-1-i][0])
+            cont[-1-i].pop(0)
         
-        
-
-        print(out)
-        
-
-        # out.append([*cont[leng-1]])
-        # cont.pop(leng-1)
-
-
-        c+=1
-
-    return out
-
-snail([[1,2,3],[4,5,6],[7,8,9]])
-
-
+print(snail([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]]))
