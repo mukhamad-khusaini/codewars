027cacddb3172b0e7c54cc33c41017124aef5dc6
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2024-01-04 13:19:31 +0700

    solved salesmen travel

---
+++ kyu6_Salesman's Travel.py
@@ -0,0 +1,34 @@
+def travel(r, zipcode):
+    addr=r.split(",")
+    o=[]
+    num=[]
+    for i in addr:
+        ad=i.split(" ")
+        zip=zipcode.split(" ")
+        if(ad[-1]==zip[-1] and ad[-2]==zip[-2]):
+            ad.pop(0)
+            ad.pop(-1)
+            ad.pop(-1)
+            o.append(" ".join(ad))
+            num.append(i.split(" ")[0])
+
+    if(r==""):
+        return zipcode+":/"
+    elif(zipcode==""):
+        return ":/"
+    else:
+        return zipcode+":"+",".join(o)+"/"+",".join(num)
+        
+
+print(travel("123 Main Street St. Louisville OH 43071,432 Main Long Road St. Louisville OH 43071,786 High Street Pollocksville NY 56432,"
+  "54 Holy Grail Street Niagara Town ZP 32908,3200 Main Rd. Bern AE 56210,1 Gordon St. Atlanta RE 13000,"
+  "10 Pussy Cat Rd. Chicago EX 34342,10 Gordon St. Atlanta RE 13000,58 Gordon Road Atlanta RE 13000,"
+  "22 Tokyo Av. Tedmondville SW 43098,674 Paris bd. Abbeville AA 45521,10 Surta Alley Goodtown GG 30654,"
+  "45 Holy Grail Al. Niagara Town ZP 32908,320 Main Al. Bern AE 56210,14 Gordon Park Atlanta RE 13000,"
+  "100 Pussy Cat Rd. Chicago EX 34342,2 Gordon St. Atlanta RE 13000,5 Gordon Road Atlanta RE 13000,"
+  "2200 Tokyo Av. Tedmondville SW 43098,67 Paris St. Abbeville AA 45521,11 Surta Avenue Goodtown GG 30654,"
+  "45 Holy Grail Al. Niagara Town ZP 32918,320 Main Al. Bern AE 56215,14 Gordon Park Atlanta RE 13200,"
+  "100 Pussy Cat Rd. Chicago EX 34345,2 Gordon St. Atlanta RE 13222,5 Gordon Road Atlanta RE 13001,"
+  "2200 Tokyo Av. Tedmondville SW 43198,67 Paris St. Abbeville AA 45522,11 Surta Avenue Goodville GG 30655,"
+  "2222 Tokyo Av. Tedmondville SW 43198,670 Paris St. Abbeville AA 45522,114 Surta Avenue Goodville GG 30655,"
+  "2 Holy Grail Street Niagara Town ZP 32908,3 Main Rd. Bern AE 56210,77 Gordon St. Atlanta RE 13000","OH 43071"))
