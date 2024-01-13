9019bb5edfa53624c95e20fa60642613c76a9845
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2024-01-13 12:30:15 +0700

    solved 2 kyu 8

---
+++ kyu8_returningstring.py
@@ -0,0 +1,10 @@
+def greet(name):
+    return "Hello, "+name+" how are you doing today?"
+
+
+print(greet("Robin"))
+
+
+# Alternative
+# def greet(name):
+#     return f'Hello, {name} how are you doing today?'
