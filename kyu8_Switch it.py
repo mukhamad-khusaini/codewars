4ae58840e895455cf3b596ed1335ce8d866ea6b5
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2024-04-02 22:49:10 +0700

    Solved switch it

---
+++ kyu8_Switch it.py
@@ -0,0 +1,18 @@
+def switch_it_up(number):
+    switcher = {
+        0: "Zero",
+        1: "One",
+        2: "Two",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine"
+    }
+
+    return switcher[number]
+
+
+print(switch_it_up(6))
