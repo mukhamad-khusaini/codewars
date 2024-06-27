23df7b0d3d1897bf2937cff400c38edf8f214e32
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2024-06-27 21:16:50 +0700

    Solved welcome

---
+++ kyu8_Welcome.py
@@ -0,0 +1,26 @@
+def greet(language):
+    vari=[ ("english", "Welcome")
+, ("czech", "Vitejte")
+, ("danish", "Velkomst")
+, ("dutch", "Welkom")
+, ("estonian", "Tere tulemast")
+, ("finnish", "Tervetuloa")
+, ("flemish", "Welgekomen")
+, ("french", "Bienvenue")
+, ("german", "Willkommen")
+, ("irish", "Failte")
+, ("italian", "Benvenuto")
+, ("latvian", "Gaidits")
+, ("lithuanian", "Laukiamas")
+, ("polish", "Witamy")
+, ("spanish", "Bienvenido")
+, ("swedish", "Valkommen")
+, ("welsh", "Croeso")
+]
+    
+    for i in vari:
+        if i[0]==language: return i[1]
+
+    return "Welcome"
+
+print(greet("english"))
