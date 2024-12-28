fd9280bf3825b3fd45136ccc29bd412f783e185e
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2024-12-28 19:42:57 +0700

    Solved For UFC fans

---
+++ kyu8_For UFC Fans.py
@@ -0,0 +1,2 @@
+def quote(fighter):
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!" if fighter.lower()=='conor mcgregor' else "I am not impressed by your performance."
