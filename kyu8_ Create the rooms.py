4da693f5934fcf0b867065a67c07a01bbf1dca4b
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2025-03-31 17:42:15 +0700

    Solved create the rooms

--- kyu8_ Create the rooms.py
+++ kyu8_ Create the rooms.py
@@ -1,17 +1,19 @@
-rooms = {
-    {
-        "name": "sa",
-        "description": "",
-        "completed": ""
-    },
-    {
-        "name": "sa",
-        "description": "",
-        "completed": ""
-    },
-    {
-        "name": "sa",
-        "description": "",
-        "completed": ""
-    }
+rooms = {}
+
+rooms["room1"] = {
+    "name": 'room1',
+    "description": "Lorem",
+    "completed": False
+}
+
+rooms["room2"] = {
+    "name": 'room2',
+    "description": "Lorem",
+    "completed": True
+}
+
+rooms["room3"] = {
+    "name": 'room3',
+    "description": "Lorem",
+    "completed": False
 }
