2cc2b5a1f6446641199549370506c73ea0b7df59
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2025-05-15 19:41:21 +0700

    Unsolved going to cinema

--- kyu7_Going to the cinema.py
+++ kyu7_Going to the cinema.py
@@ -1,16 +1,34 @@
 import math
 def movie(card, ticket, perc):
-    time=0
-    t=0
-    p=card
-    vem=0
-    while math.ceil(p)>t:
-        if vem==0: vem=ticket*perc
-        else: vem=vem*perc
-        p+=vem
-        t+=ticket
-        time+=1
+    def sysA(price, time):
+        return price*time
     
-    return time
+    def sysB(cardPrice,price,time,percent):
+        total=0
+        priceOverTime=0
+        i=0
+        while i<time:
+            if priceOverTime==0: 
+                priceOverTime=price*percent
+                total+=priceOverTime
+                i+=1
+            else:
+                priceOverTime=priceOverTime*percent
+                total+=priceOverTime
+                i+=1
+        
+        return total+cardPrice
+
+    syA=0
+    syB=0
+    i=1
+    while math.ceil(syB)>=syA:
+        syA+=sysA(ticket,i)
+        syB+=sysB(card,ticket,i,perc)
+        i+=1
+    
+    return i
+
+
 
 print(movie(500, 15, 0.9))
