c16c68dd3ff50030d9f4541c0c6684c5b11a4e56
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2025-05-16 19:51:46 +0700

    Solved going to cinema with copilot's aid

--- kyu7_Going to the cinema.py
+++ kyu7_Going to the cinema.py
@@ -1,34 +1,16 @@
 import math
 def movie(card, ticket, perc):
-    def sysA(price, time):
-        return price*time
-    
-    def sysB(cardPrice,price,time,percent):
-        total=0
-        priceOverTime=0
-        i=0
-        while i<time:
-            if priceOverTime==0: 
-                priceOverTime=price*percent
-                total+=priceOverTime
-                i+=1
-            else:
-                priceOverTime=priceOverTime*percent
-                total+=priceOverTime
-                i+=1
-        
-        return total+cardPrice
-
-    syA=0
-    syB=0
+    sysA=0
+    sysB=0
     i=1
-    while math.ceil(syB)>=syA:
-        syA+=sysA(ticket,i)
-        syB+=sysB(card,ticket,i,perc)
+    while math.ceil(sysB)>=sysA:
+        sysA=ticket*i
+        sysB=sum([ticket*(perc**c) for c in range(1,i+1)])
+        sysB+=card
+        if math.ceil(sysB)<sysA: return i
         i+=1
-    
-    return i
 
+    return i
 
 
 print(movie(500, 15, 0.9))
