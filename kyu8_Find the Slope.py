7eeb9fd7f1ec75b2800c3ba6231a16f2df432c06
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2025-06-07 19:39:42 +0700

    Maybe test broken

--- kyu8_Find the Slope.py
+++ kyu8_Find the Slope.py
@@ -1,3 +1,3 @@
 import math
 def find_slope(points):
-    return 'undefined' if (points[2]-points[0])==0 else f"{math.trunc((points[3]-points[1])/(points[2]-points[0]))}"
+    return "undefined" if points[2]-points[0]==0 else str(math.trunc((points[3]-points[1])/(points[2]-points[0])))
