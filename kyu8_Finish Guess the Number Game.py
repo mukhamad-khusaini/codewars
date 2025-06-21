883af5d24ded5438edaa9a5f56f5d86c87e8f340
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2025-06-21 19:21:19 +0700

    Solved finish guess the number game with AI help

--- kyu8_Finish Guess the Number Game.py
+++ kyu8_Finish Guess the Number Game.py
@@ -2,10 +2,13 @@ class Guesser:
     def __init__(self, number, lives):
         self.number = number
         self.lives = lives
-  
-    def guess(self,n):
-        if self.lives==0: raise ValueError("Omae wa mo shindeiru")
-        elif(n==self.number): 
-            self.lives = self.lives-1
+
+    def guess(self, n):
+        if self.lives <= 0:
+            raise ValueError("You have no lives left! Game Over.")
+
+        if n == self.number:
             return True
-        else: return False
+        else:
+            self.lives -= 1
+            return False
