16b3f8a6f772e0287862244aec4bf74813d186f6
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2025-08-21 19:17:48 +0700

    Solved consecutive items

--- kyu7_Consecutive items.py
+++ kyu7_Consecutive items.py
@@ -1,4 +1,4 @@
 def consecutive(arr, a, b):
-    return f"{a}{b}" in "".join([str(i) for i in arr]) or f"{b}{a}" in "".join([str(i) for i in arr])
+    return f"{a} {b}" in " ".join([str(i) for i in arr]) or f"{b} {a}" in " ".join([str(i) for i in arr])
 
 print(consecutive([1,2,3,4,5,6],6,3))
