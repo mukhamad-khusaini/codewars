24c882a8cbaf5c12926859e3c36a32eedbc954ac
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2025-08-23 19:25:16 +0700

    Solved check three and two

---
+++ kyu7_Check three and two.py
@@ -0,0 +1,2 @@
+def check_three_and_two(array):
+    return True if 3 in [array.count("a"),array.count("b"),array.count("c")] and 2 in [array.count("a"),array.count("b"),array.count("c")] else False
