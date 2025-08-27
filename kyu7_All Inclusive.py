cf260a4be5408f98455412783d9ee5469da696f9
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2025-08-27 19:33:21 +0700

    Solved all inclusive

--- kyu7_All Inclusive.py
+++ kyu7_All Inclusive.py
@@ -1,4 +1,11 @@
 def contain_all_rots(strng, arr):
     rot=[]
     for i in range(len(strng)-1):
-        rot.append([])
+        rot.append(strng[i+1:]+strng[:i+1])
+    
+    rot.append(strng)
+
+    return set(rot).issubset(arr)
+
+print(contain_all_rots("bsjq",["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]))
+# print("abcd"[:2])
