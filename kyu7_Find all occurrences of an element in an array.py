4610c3442fde16e0bf39199011a7ce5e78f761d8
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2025-08-29 19:47:40 +0700

    Solved find all occurences

--- kyu7_Find all occurrences of an element in an array.py
+++ kyu7_Find all occurrences of an element in an array.py
@@ -1,12 +1,4 @@
 def find_all(array, n):
-    occ=[]
-    arr=array
-    for i in range(len(arr)):
-        try:
-            occ.append(arr.index(n))
-            arr=arr[arr.index(n)+1:]
-            print(arr)
-        except:
-            return occ
+    return [i for i in range(len(array)) if array[i]==n]
 
 print(find_all([1,2,3,4,5,3,5],3))
