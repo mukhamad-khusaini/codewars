f3a3efa3600979a2fa012fe5a35df41bd5338b04
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2025-09-01 22:16:11 +0700

    Solved remove the parentheses

--- kyu6_Remove the parentheses.py
+++ kyu6_Remove the parentheses.py
@@ -1,9 +1,23 @@
 def remove_parentheses(st):
-    result=[]
-    content=st.split("(")
-    while content:
-        result.append(content[0])
-        content.pop(0)
-        print(content)
+    delete=[]
+    currentPare=[]
+    rejector=0
+    for i in range(len(st)):
+        if st[i]=="(":
+            if currentPare:
+                currentPare.append(i)
+                rejector+=1
+            else:
+                currentPare.append(i)
+        elif st[i]==")":
+            if rejector!=0:
+                rejector-=1
+                currentPare.append(i)
+            else:
+                currentPare.append(i)
+                delete+=list(range(currentPare[0],currentPare[-1]+1))
+                currentPare=[]
+    
+    return "".join([st[i] for i in range(len(st)) if i not in delete])
 
-# print(remove_parentheses("in(g) ngarso(suy(sdasd)n)Tulodo"))
+print(remove_parentheses("(tanya) (jawab) (saja) (hai(hai))f"))
