208aa0bf95d81d820cbbec81252692f0384f4220
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2025-09-07 19:33:53 +0700

    Unsolved plus minus

---
+++ kyu7_Plus - minus - plus - plus.py
@@ -0,0 +1,23 @@
+def catch_sign_change(lst):
+    global sign
+    sign=""
+    change=0
+
+    for i in range(len(lst)):
+        if i == 0:
+            if lst[i]>=0:
+                sign="p"
+            else:
+                sign="n"
+        elif lst[i] >= 0:
+            if sign == "p": continue
+            else: 
+                change+=1
+                sign="n"
+        elif lst[i] < 0:
+            if sign == "n": continue
+            else: 
+                change+=1
+                sign="p"
+        
+    return change
