dde4c57cb08a28433197746c10fa3659c7b33f8c
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2025-09-10 19:15:54 +0700

    Solved scalling squared strings

--- kyu7_Scaling Squared Strings.py
+++ kyu7_Scaling Squared Strings.py
@@ -1,4 +1,5 @@
 def scale(strng, k, v):
+    if not strng: return ""
     ns=strng.split("\n")
     kh=[]
     for i in ns:
@@ -6,11 +7,11 @@ def scale(strng, k, v):
         for x in i:
             temp.append(x*k)
         kh.append("".join(temp))
-    
+
     vv=[]
     for i in kh:
-        vv.append(i*v)
-    
+        vv.append("\n".join([i for x in range(v)]))
+  
     return "\n".join(vv)
 
 print(scale("abcd\nefgh\nijkl\nmnop", 2,3))
