63da1cc189b061cc7ed8029917af6602367b221d
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2025-09-21 19:22:47 +0700

    Solved over the road

--- kyu7_over the road.py
+++ kyu7_over the road.py
@@ -1,2 +1,19 @@
 def over_the_road(address, n):
-    return 
+    if address%2!=0:
+        ad=address+1
+        mid=n+0.5
+        delta=abs(ad-mid)
+        if ad>mid:
+            return int(mid-delta+1)
+        else:
+            return int(mid+delta+1)
+    else:
+        ad=address-1
+        mid=n+0.5
+        delta=abs(ad-mid)
+        if ad>mid:
+            return int(mid-delta-1)
+        else:
+            return int(mid+delta-1)
+
+print(over_the_road(8,6))
