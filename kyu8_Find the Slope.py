3fb825a7ed3b64b686e761a319668ff1676076aa
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2025-09-27 19:16:16 +0700

    Unsolved find the slope

--- kyu8_Find the Slope.py
+++ kyu8_Find the Slope.py
@@ -1,3 +1,3 @@
 import math
 def find_slope(points):
-    return "undefined" if points[2]-points[0]==0 else str(math.trunc((points[3]-points[1])/(points[2]-points[0])))
+    return str(int((points[3]-points[1])/(points[2]-points[0]))) if points[2]-points[0] != 0 else "undefined"
