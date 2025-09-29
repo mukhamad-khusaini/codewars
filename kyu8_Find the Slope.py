7fb0014f8d3620568309eec263638013c22558d3
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2025-09-29 19:45:11 +0700

    I thing find the slope kata is broken

--- kyu8_Find the Slope.py
+++ kyu8_Find the Slope.py
@@ -1,3 +1,3 @@
 import math
 def find_slope(points):
-    return str(int((points[3]-points[1])/(points[2]-points[0]))) if points[2]-points[0] != 0 else "undefined"
+    return str(math.ceil((points[3]-points[1])/(points[2]-points[0]))) if abs(points[2]-points[0]) != 0 else "undefined"
